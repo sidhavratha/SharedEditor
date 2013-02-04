90ce7fa3cb4f3c232f2694815a9437391841f792
Sidhavratha Kumar <sid@ubuntu.(none)>
2013-02-04 12:04:00 -0800

    port rectified

--- sharededit.js
+++ sharededit.js
@@ -91,6 +91,12 @@ delete sessions[editorId];
 }
 },1000*60*1);
 
-app.listen(8124);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+//app.listen(8124);
 console.log('Starting application');
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:'+port);
